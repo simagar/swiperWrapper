50b134e6ab747bad90da1a5a629d0ba1f0155504
@samanfattahi <samanft752@gmail.com>
2024-09-10 16:49:25 +0330

    init swipperWrapper

---
+++ components/SwiperWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class=" w-full px-2 py-2">
+    <swiper
+        v-bind="options"
+    >
+      <swiper-slide v-for="item in items" class="">
+        <slot :data="item" name="slide"></slot>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type PropType from 'vue'
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  },
+  options: {
+    type: Object as PropType<object>
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
